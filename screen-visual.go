25fb27a99c47b73793c357122ed9e639797a7fa5
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-24 20:27:17 -0300

    Mudança do nome da função Line para Lineln

--- screen-visual.go
+++ screen-visual.go
@@ -2,8 +2,8 @@ package screen_visual
 
 import "fmt"
 
-// Draw lines on screen
-func Line(char string, size int) {
+// Draw lines on screen with a line break at the end.
+func Lineln(char string, size int) {
 
 	line := ""
 	for i := 0; i < size; i++ {
@@ -12,3 +12,4 @@ func Line(char string, size int) {
 	fmt.Println(line)
 
 }
+
